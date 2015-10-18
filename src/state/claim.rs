9290dc62008a1a6dbefee20f51700446d7885235
Alex Iadicicco <alex@ajitek.net>
2015-10-17 20:54:32 -0700

    Write more proofs about claim objects

--- src/state/claim.rs
+++ src/state/claim.rs
@@ -3,7 +3,36 @@
 
 //! Claim handling
 //!
-//! Claims are the optimistic locks that make `ircd-oxide` work.
+//! Claims are the optimistic locks that make `ircd-oxide` work. They are used
+//! in places where data presented to users appears to exist in a shared global
+//! namespace, such as channel names and nicknames. For such data, `ircd-oxide`
+//! attempts to make a "claim" on that data. Data can only be claimed for things
+//! that are guaranteed to be unique (the "owner"). Examples of things that are
+//! guaranteed to be unique are channel IDs and user identities.
+//!
+//! Claims themselves are simple: you either have a claim over some data, or you
+//! don't. The difficult part is that your claim to that data may be superseded
+//! at any moment. This module only deals with detecting such events, and
+//! ensuring that conflicts are reconciled in the same way across all nodes.
+//! Handling the loss of a claim is the responsibility of the user the claim is
+//! issued to.
+//!
+//! Take nicknames as a simple example. When a user wants to use a nickname for
+//! which the server they are connected to sees no claim, the server creates a
+//! claim and broadcasts it. If at any point the claim is superseded, the server
+//! must indicate to the client that they are no longer using that nickname,
+//! usually with something like a forced nickname change to a unique ID.
+//!
+//! Expirations are a slightly more subtle feature, and act like "tombstones".
+//! When a user releases their claim to some data, we can't just forget the
+//! claim existed. Since older claims always win, we can only create a newer
+//! claim using a prior expiration as justification. If we broadcast a newer
+//! claim without justifying it, we are relying on all receiving servers having
+//! no older claim. Since we cannot reliably verify at any time that all servers
+//! have seen an expiration, we keep track of the expiration for all data.
+//!
+//! In the future, to prevent the unbounded growth of expired claims, we may use
+//! some kind of strong consistency to clean up old expirations.
 
 use std::marker::PhantomData;
 
@@ -12,7 +41,7 @@ use state::Clock;
 use state::Id;
 use state::StateItem;
 
-/// A claim object
+/// A claim object.
 ///
 /// To explain how the merge rules for claims work, consider a history of all
 /// information about a claim, i.e. the set of all new claims and claim
@@ -37,6 +66,41 @@ use state::StateItem;
 ///   * The expiration clocks do not match. This means that we have two claims
 ///     and two expirations to reconcile. We then simply pick the newer of the
 ///     two expirations, and the older claim to occur after that expiration.
+///
+/// ***Claim.*** The following two models of "claims" as state items will
+/// produce the same active claim after a merge:
+///
+///   1. The "claim object" is a set of all timestamped claim attempts and
+///      timestamped expirations. The active claim is the oldest claim that
+///      comes after the newest expiration. If no such claim exists, there is no
+///      active claim. Merging claim objects is simply set union.
+///
+///   2. The "claim object" is a single timestamped claim attempt and a single
+///      timestamped expiration. The claim is active if the expiration is older
+///      than the claim attempt. Merging claim objects is performed using the
+///      method described above.
+///
+/// ***Proof.*** Consider two claim objects to be merged under the first model,
+/// call them *A* and *B*. Each claim object has a newer expiration, *Ea* and
+/// *Eb* respectively. Using the method described, we can pick an active claim
+/// for each set, call them *Ca* and *Cb* respectively. When we merge the sets,
+/// one of *Ea* or *Eb* will be the newer expiration. WLOG, let *Ea* be the
+/// newer expiration. At this point, the active claim in the new set will be
+/// *only* either *Ca* or *Cb*. To make a contradiction, suppose in the newly
+/// merged set that some claim *Cx*, distinct from *Ca* or *Cb*, is the active
+/// claim. In other words, T(*Cx*) < T(*Ca*) and T(*Cx*) < T(*Cb*), where T(*e*)
+/// indicates the (unique) time of event *e*. Note also that T(*Eb*) < T(*Ea*) <
+/// T(*Cx*). If *Cx* were in starting set *A*, then we would have T(*Ea*) <
+/// T(*Cx*) < T(*Ca*), and *Ca* would not be the valid claim from that as
+/// defined above. Similarly, if *Cx* were in starting set *B*, then *Cb* would
+/// not be the valid claim from that set. Since *Cx* is in neither *A* nor *B*,
+/// it is not in the union of *A* and *B*, and so cannot exist as we have
+/// defined it. Therefore, the newly valid claim after the union must be either
+/// *Ca* or *Cb*.
+///
+/// I will finish this proof later, but you hopefully see where I'm going with
+/// this. We only need to store and transmit *Ea*, *Eb*, *Ca*, and *Cb*, to
+/// determine the new expiration and valid claim.
 #[derive(Debug)]
 pub struct Claim<Owner: 'static, Over: 'static> {
     expired: Clock,

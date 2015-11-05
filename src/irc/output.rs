fe6ef035fb8bf10267bbae9d90d3638912ee1b15
Alex Iadicicco <alex@ajitek.net>
2015-11-04 22:12:25 -0800

    Prefer more generic IrcWriter on things implementing io::Write

--- src/irc/output.rs
+++ src/irc/output.rs
@@ -6,6 +6,7 @@
 
 //! IRC output handling
 
+use std::io;
 use std::fmt::Arguments;
 use std::convert::AsRef;
 
@@ -13,7 +14,7 @@ use std::convert::AsRef;
 pub trait IrcWriter {
     /// Writes an IRC line with the verb and format arguments. Use the `irc!`
     /// macro for calling this.
-    fn irc<'a, V>(&mut self, verb: &V, args: Arguments<'a>)
+    fn irc<'a, V>(&mut self, verb: &V, args: Arguments<'a>) -> io::Result<()>
     where V: AsRef<[u8]>;
 }
 
@@ -26,43 +27,20 @@ macro_rules! irc {
     }
 }
 
-impl IrcWriter for Vec<u8> {
-    fn irc<'a, V: AsRef<[u8]>>(&mut self, verb: &V, args: Arguments<'a>) {
-        self.extend(verb.as_ref());
-
-        let s = format!(" {}", args);
-
-        if s.len() > 1 { // 1 for the space
-            self.extend(s.as_bytes());
-        }
-
-        self.extend("\r\n".as_bytes());
+impl<W> IrcWriter for W where W: io::Write {
+    fn irc<'a, V>(&mut self, verb: &V, args: Arguments<'a>) -> io::Result<()>
+    where V: AsRef<[u8]> {
+        try!(self.write(verb.as_ref()));
+        try!(self.write_fmt(format_args!(" {}\r\n", args)));
+        Ok(())
     }
-}
-
-#[test]
-fn write_no_space() {
-    let mut s: Vec<u8> = Vec::new();
-
-    irc!(&mut s, "PING", "");
-
-    assert_eq!("PING\r\n".as_bytes(), &s[..]);
-}
-
-#[test]
-fn write_space() {
-    let mut s: Vec<u8> = Vec::new();
-
-    irc!(&mut s, "PING", ":pong");
-
-    assert_eq!("PING :pong\r\n".as_bytes(), &s[..]);
 }
 
 #[test]
 fn write_format() {
     let mut s: Vec<u8> = Vec::new();
 
-    irc!(&mut s, "PING", ":{}", 3);
+    irc!(&mut s, "PING", ":{}", 3).unwrap();
 
     assert_eq!("PING :3\r\n".as_bytes(), &s[..]);
 }

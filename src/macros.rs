2e66ac6abdd5f0eb2162231c3392e3996fab38c9
Alex Iadicicco <alex@ajitek.net>
2015-11-13 00:14:09 -0800

    Remove that horrible `irc!` macro
    I need to come up with a better way of sending messages

--- src/macros.rs
+++ src/macros.rs
@@ -6,32 +6,4 @@
 
 //! Macros
 
-macro_rules! irc {
-    ($writer:expr,) => {
-        ::std::io::Write::write($writer, b"\r\n")
-    };
-
-    ($writer:expr, $arg:expr,) => {
-        ::std::io::Write::write(
-            $writer,
-            ::std::convert::AsRef::<[u8]>::as_ref(&$arg)
-        )
-    };
-
-    ($writer:expr, $arg:expr) => {
-        match irc!($writer, $arg,) {
-            Err(e) => Err(e),
-            Ok(_) => irc!($writer,)
-        }
-    };
-
-    ($writer:expr, $arg:expr, $($args:tt)*) => {
-        match irc!($writer, $arg,) {
-            Err(e) => Err(e),
-            Ok(_) => match ::std::io::Write::write($writer, b" ") {
-                Err(e) => Err(e),
-                Ok(_) => irc!($writer, $($args)*),
-            }
-        }
-    }
-}
+macro_rules! irc { }
